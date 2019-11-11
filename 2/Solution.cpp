83a2b9d8f8b60be01a142aef9f4c8bdef8bd9e3f
xiaowangfei <wangqi0725@gmail.com>
2019-11-11 09:15:52 -0600

    ac create a new list

---
+++ 2/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr=dummy;
+        int carry=0;
+        while(l1 || l2 || carry) {
+            int val = carry;
+            if(l1) {
+                val += l1 -> val;
+                l1 = l1 -> next;
+            }
+            if(l2){
+                val += l2 -> val;
+                l2 = l2 -> next;
+            }
+            curr -> next = new ListNode(val%10);
+            curr = curr -> next;
+            carry = val/10;
+        }
+        return dummy -> next;
+    }
+};
