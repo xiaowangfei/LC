408c5105fd834183c486f7727ac0171f181bb103
xiaowangfei <wangqi0725@gmail.com>
2019-11-13 21:51:19 -0600

    ac

---
+++ 28/Solution.cpp
@@ -0,0 +1,17 @@
+int strStr(string haystack, string needle) {
+    if(needle.empty()){
+        return 0;
+    }
+    for(int i = 0; i < haystack.size(); i++){
+        for(int j = 0; j < needle.size(); j++){
+            if(i + j == haystack.size() || haystack[i + j] != needle[j]){
+                break;
+            }
+            if(j == needle.size() - 1) {
+                return i;
+            }
+        }
+    }
+    return -1;
+
+}
