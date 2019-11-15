01b683c4a50d648b2605380665eaea6daa258c53
xiaowangfei <wangqi0725@gmail.com>
2019-11-14 21:49:55 -0600

    ac

---
+++ 125/Solution.cpp
@@ -0,0 +1,37 @@
+ bool isPalindrome(string s) {
+     int start = 0;
+     int end = s.size() - 1;
+     while(start < end){
+         while(start < s.size() && !isalnum(s[start])){
+             start++;
+         }
+         while(end >= 0 && !isalnum(s[end])){
+             end--;
+         }
+         if(start < end && tolower(s[start]) != tolower(s[end])){
+             return false;
+         }
+         start++;
+         end--;
+     }
+     return true;
+ }
+
+ bool isPalindrome(string s) {
+     int start = 0;
+     int end = s.size() - 1;
+     while(start < end){
+         while(start < end && !isalnum(s[start])){
+             start++;
+         }
+         while(start < end && !isalnum(s[end])){
+             end--;
+         }
+         if(tolower(s[start]) != tolower(s[end])){
+             return false;
+         }
+         start++;
+         end--;
+     }
+     return true;
+ }
