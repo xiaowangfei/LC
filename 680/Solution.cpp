47283800a27a88d4c84e31aa309ffc2e79fb4645
xiaowangfei <56806687+xiaowangfei@users.noreply.github.com>
2019-11-28 17:22:46 -0600

    680. Valid Palindrome II

--- 680/Solution.cpp
+++ 680/Solution.cpp
@@ -23,3 +23,25 @@ bool helper(int l, int r, string s){
     }
     return true;
 }
+
+//Shorter
+bool validPalindrome(string s) {
+    int l = 0;
+    int r = s.size() - 1;
+    while(l < r){
+        if(s[l++] != s[r--]){
+            return helper(l - 1, r, s) || helper(l, r + 1, s);
+        }            
+    }
+    return true;        
+}
+bool helper(int l, int r, string s){
+    while(l < r){
+        if(s[l++] != s[r--]){
+            return false;
+        }
+    }
+    return true;
+}
+
+//Write helper function
