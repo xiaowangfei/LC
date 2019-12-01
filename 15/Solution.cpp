85b02f4587d390eede698976a462760408b9fe5b
xiaowangfei <56806687+xiaowangfei@users.noreply.github.com>
2019-12-01 17:46:49 -0600

    15. 3Sum

--- 15/Solution.cpp
+++ 15/Solution.cpp
@@ -31,3 +31,63 @@ vector<vector<int>> threeSum(vector<int>& nums) {
     }
     return result;  
 }
+//Practice using one Hash. Mind ordering for duplciate!
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> result;
+    if(nums.size() < 3){
+        return result;
+    }
+    map<int, int> H;
+    for(int i = 0; i < nums.size(); i++){
+        if(H.find(nums[i]) == H.end()){
+            H[nums[i]] = 1;
+        }
+        else{
+            H[nums[i]] += 1;
+        }
+    }
+    sort(nums.begin(), nums.end());
+    for(int i = 0; i < nums.size(); i++){
+        if(i != 0 && nums[i] == nums[i - 1]){  //Remove duplicate is needed even for Hash method
+            continue;
+        }
+        for(int j = i + 1; j < nums.size(); j++){
+            if(j != i + 1 && nums[j] == nums[j - 1]){
+                continue;
+            }
+            if(H.find(0 - nums[i] - nums[j]) == H.end()){
+                continue;
+            }
+            if(nums[i] == nums[j]){
+                if(nums[j] == 0 - nums[i] - nums[j] && H[0 - nums[i] - nums[j]] >= 3){
+                        vector<int> temp(3, nums[i]);
+                        result.push_back(temp);
+                }
+                else if(nums[j] < 0 - nums[i] - nums[j]){
+                    vector<int> temp;
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(0 - nums[i] - nums[j]);
+                    result.push_back(temp);
+                }
+            }
+            else{
+                if(nums[j] ==0 - nums[i] - nums[j] && H[0 - nums[i] - nums[j]] >= 2){
+                    vector<int> temp;
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(0 - nums[i] - nums[j]);
+                    result.push_back(temp);
+                }
+                else if(nums[j] < 0 - nums[i] - nums[j]){
+                    vector<int> temp;
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(0 - nums[i] - nums[j]);
+                    result.push_back(temp);
+                }
+            }
+        }        
+    }
+    return result;  
+}
