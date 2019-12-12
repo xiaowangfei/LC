fbcc4f49423a1616bf8695d8b8215068203b330b
xiaowangfei <56806687+xiaowangfei@users.noreply.github.com>
2019-12-12 11:38:52 -0600

    215. Kth Largest Element in an Array

---
+++ 215/Solution.cpp
@@ -0,0 +1,28 @@
+//Use forward two pointers
+int findKthLargest(vector<int>& nums, int k) {
+    return helper(nums, 0, nums.size() - 1, k);
+}
+int helper(vector<int>& nums, int start, int end, int k){
+    int idx = start;
+    int key = nums[end];
+    for(int i = start; i < end; i++){
+        if(nums[i] >= key){
+            int temp = nums[i];
+            nums[i] = nums[idx];
+            nums[idx] = temp;
+            idx++;
+        }
+    }
+    nums[end] = nums[idx];
+    nums[idx] = key;
+    //Or use len = idx - start + 1
+    if(idx + 1 == k){
+        return nums[idx];
+    }
+    else if(idx + 1 > k){
+        return helper(nums, start, idx - 1, k);
+    }
+    else{
+        return helper(nums, idx + 1, end, k);
+    }
+} 
