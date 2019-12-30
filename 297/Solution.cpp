d80b56e5072515c1e4c7a08254ed1e2767df8819
xiaowangfei <56806687+xiaowangfei@users.noreply.github.com>
2019-12-29 23:17:34 -0600

    297. Serialize and Deserialize Binary Tree

--- 297/Solution.cpp
+++ 297/Solution.cpp
@@ -92,3 +92,28 @@ void dfs_de(TreeNode* root, istringstream& ss){
     }
 }
 
+// DFS Concise version. Note recursion return desired type!
+// Encodes a tree to a single string.
+string serialize(TreeNode* root) {
+    if(root == NULL) return "# ";
+    return to_string(root -> val) + " " + serialize(root -> left) + serialize(root -> right);
+}
+
+// Decodes your encoded data to tree.
+TreeNode* deserialize(string data) {
+    istringstream ss(data);
+    return dfs_de(ss);  
+}
+    
+TreeNode* dfs_de(istringstream& ss){
+    string s;
+    ss >> s;
+    if(s == "#"){
+        return NULL;
+    }
+    TreeNode* new_ptr = new TreeNode(stoi(s));
+    new_ptr -> left = dfs_de(ss);
+    new_ptr -> right = dfs_de(ss);
+    return new_ptr;
+}
+
