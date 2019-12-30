906e703a8237cf195805b7be143f3d9e7e275989
xiaowangfei <56806687+xiaowangfei@users.noreply.github.com>
2019-12-29 22:57:00 -0600

    297. Serialize and Deserialize Binary Tree

--- 297/Solution.cpp
+++ 297/Solution.cpp
@@ -24,22 +24,22 @@ string serialize(TreeNode* root) {
 // You can't build the tree first(creating left/right child), and then decide whether it should be NULL or set value.
 // So, it has to first read the string, then create left/right child depending on the string value.
 TreeNode* deserialize(string data) {
-    if(data == "# ") return NULL;
-    istringstream sin(data);
+    istringstream ss(data);
     string s;
-    sin >> s;
+    ss >> s;
+    if(s == "#") return NULL;
     TreeNode* root = new TreeNode(stoi(s));
     queue<TreeNode*> bfs_q;
     bfs_q.push(root);
     while(!bfs_q.empty()){
         TreeNode* front = bfs_q.front();
         bfs_q.pop();
-        sin >>s;
+        ss >>s;
         if(s != "#"){
             front -> left = new TreeNode(stoi(s));
             bfs_q.push(front -> left);
         }
-        sin>>s;
+        ss>>s;
         if(s != "#"){
             front -> right = new TreeNode(stoi(s));
             bfs_q.push(front -> right);
@@ -47,3 +47,48 @@ TreeNode* deserialize(string data) {
     }
     return root;
 }
+
+//DFS pre-order
+// Encodes a tree to a single string.
+string serialize(TreeNode* root) {
+    string ans;
+    dfs(root, ans);
+    return ans;
+}
+    
+void dfs(TreeNode* root, string & ans){
+    if(root == NULL){
+        ans += "# ";
+        return;
+    }
+    ans += to_string(root -> val) + " ";
+    dfs(root -> left, ans);
+    dfs(root -> right, ans);
+}
+    
+
+// Decodes your encoded data to tree.
+TreeNode* deserialize(string data) {
+    istringstream ss(data);
+    string s;
+    ss >> s;
+    if(s == "#") return NULL;
+    TreeNode* root = new TreeNode(stoi(s));
+    dfs_de(root, ss);
+    return root;    
+}
+    
+void dfs_de(TreeNode* root, istringstream& ss){
+    string s;
+    ss >> s;
+    if(s != "#"){
+        root -> left = new TreeNode(stoi(s));
+        dfs_de(root -> left, ss);
+    }
+    ss >> s;
+    if(s != "#"){
+        root -> right = new TreeNode(stoi(s));
+        dfs_de(root -> right, ss);
+    }
+}
+
