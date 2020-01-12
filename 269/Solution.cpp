8f00b8e48d5e83621b1447f888aa83c624f232fa
xiaowangfei <56806687+xiaowangfei@users.noreply.github.com>
2020-01-12 11:45:30 -0600

    269. Alien Dictionary

--- 269/Solution.cpp
+++ 269/Solution.cpp
@@ -44,3 +44,58 @@ bool firstDiff(string a, string b, pair<char, char>& edge){
     }
     return false;
 }
+
+//DFS
+string alienOrder(vector<string>& words) {
+    unordered_map<char, vector<char>> graph;
+    for(string word : words){
+        for(char c : word){
+            graph[c] = vector<char>();
+        }
+    }
+    for(int i = 0; i < words.size() - 1; i++){
+        pair<char, char> edge;
+        if(firstDiff(words[i], words[i + 1], edge)){
+            graph[edge.first].push_back(edge.second);
+        }
+    }
+    
+    unordered_set<char> discover;
+    unordered_set<char> visited;
+    stack<char> sans;
+    for(char c = 'a'; c <= 'z'; c++){
+        if(!dfs(c, graph, discover, visited, sans)){
+            return "";
+        }
+    }
+    string ans = "";
+    while(!sans.empty()){
+        ans += sans.top();
+        sans.pop();
+    }
+    return ans;
+}
+bool dfs(char c, unordered_map<char, vector<char>>& graph, unordered_set<char>& discover, unordered_set<char>& visited, stack<char>& sans){
+    if(graph.find(c) == graph.end()) return true;
+    if(visited.find(c) != visited.end()) return true;
+    if(discover.find(c) != discover.end()) return false;
+    discover.insert(c);
+    for(char neib : graph[c]){
+        if(!dfs(neib, graph, discover, visited, sans)){
+            return false;
+        }
+    }
+    sans.push(c);
+    visited.insert(c);
+    return true;   
+}
+
+bool firstDiff(string a, string b, pair<char, char>& edge){
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]){
+            edge = make_pair(a[i], b[i]);
+            return true;
+        }
+    }
+    return false;
+}
