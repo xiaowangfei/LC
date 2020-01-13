32e2d113ec55e8236e2312001ef4a491b3ad8926
xiaowangfei <56806687+xiaowangfei@users.noreply.github.com>
2020-01-12 21:48:36 -0600

    257. Binary Tree Paths

--- 257/Solution.cpp
+++ 257/Solution.cpp
@@ -1,3 +1,4 @@
+//Recursion
 vector<string> binaryTreePaths(TreeNode* root) {
     vector<string> ans;
     string path =  "";
@@ -16,4 +17,58 @@ void helper(TreeNode* root, string path, vector<string>& ans){
         helper(root -> left, path, ans);
         helper(root -> right, path, ans);
     }
+}
+//BFS1
+vector<string> binaryTreePaths(TreeNode* root) {
+    vector<string> ans;
+    if(root == NULL) return ans;
+    queue<TreeNode*> bfs_q;
+    queue<string> str_q;
+    str_q.push(to_string(root -> val));
+    bfs_q.push(root);
+    while(!bfs_q.empty()){
+        TreeNode* front = bfs_q.front();
+        string front_str =  str_q.front();
+        bfs_q.pop();
+        str_q.pop();
+        if(front -> left == NULL && front -> right == NULL){
+            ans.push_back(front_str);
+        }
+        if(front -> left) {
+            bfs_q.push(front -> left);
+            str_q.push(front_str + "->" + to_string(front -> left -> val));
+        }
+        if(front -> right){
+            bfs_q.push(front -> right);
+            str_q.push(front_str + "->" + to_string(front -> right -> val));
+        }
+        
+    }
+    return ans;
+}
+//BFS2
+vector<string> binaryTreePaths(TreeNode* root) {
+    vector<string> ans;
+    if(root == NULL) return ans;
+    queue<TreeNode*> bfs_q;
+    map<TreeNode*, string> strmap;
+    strmap[root] = "";
+    bfs_q.push(root);
+    while(!bfs_q.empty()){
+        TreeNode* front = bfs_q.front();
+        bfs_q.pop();
+        if(front -> left == NULL && front -> right == NULL){
+            ans.push_back(strmap[front] + to_string(front -> val));
+        }
+        if(front -> left) {
+            bfs_q.push(front -> left);
+            strmap[front -> left] = strmap[front] + to_string(front -> val) + "->";
+        }
+        if(front -> right){
+            bfs_q.push(front -> right);
+            strmap[front -> right] = strmap[front] + to_string(front -> val) + "->";
+        }
+        
+    }
+    return ans;
 }
