00324a208e36842df8ffa4cff92de78227ea2626
xiaowangfei <56806687+xiaowangfei@users.noreply.github.com>
2020-01-15 22:51:18 -0600

    113. Path Sum II

--- 113/Solution.cpp
+++ 113/Solution.cpp
@@ -18,3 +18,21 @@ vector<vector<int>> pathSum(TreeNode* root, int sum) {
     }
     return ans;
 }
+
+//Traverse
+vector<vector<int>> pathSum(TreeNode* root, int sum) {
+    vector<vector<int>> ans;
+    vector<int> path;
+    helper(root, sum, path, ans);
+    return ans;
+}
+void helper(TreeNode* root, int sum, vector<int>& path, vector<vector<int>>& ans){
+    if(root == NULL) return;
+    path.push_back(root -> val);
+    if(root -> left == NULL && root -> right == NULL && sum == root -> val){
+        ans.push_back(path);
+    }
+    helper(root -> left, sum - root -> val, path, ans);
+    helper(root -> right, sum - root -> val, path, ans);
+    path.pop_back();
+}
