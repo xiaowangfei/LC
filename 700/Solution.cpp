f5a0b5d1e220dfaece38e06c08b72bd28e288667
xiaowangfei <56806687+xiaowangfei@users.noreply.github.com>
2020-01-18 17:33:24 -0600

    700. Search in a Binary Search Tree

---
+++ 700/Solution.cpp
@@ -0,0 +1,35 @@
+//Recursion
+TreeNode* searchBST(TreeNode* root, int val) {
+    if(root == NULL || root -> val == val) return root;
+    return root -> val > val ? searchBST(root -> left, val) : searchBST(root -> right, val);
+}
+
+//Iterative
+TreeNode* searchBST(TreeNode* root, int val) {
+    if(root == NULL) return NULL;
+    stack<TreeNode*> S;
+    S.push(root);
+    while(!S.empty()){
+        TreeNode* top = S.top();
+        S.pop();
+        if(top -> val == val){
+            return top;
+        }
+        if(top -> val > val && top -> left){
+            S.push(top -> left);
+        }
+        if(top -> val < val && top -> right){
+            S.push(top -> right);
+        }            
+    }
+    return NULL;
+}
+
+//Don't need a stack!
+TreeNode* searchBST(TreeNode* root, int val) {
+    while(root){
+        if(root -> val == val) return root;
+        root = root -> val < val ? root -> right : root -> left;
+    }
+    return NULL;
+}
