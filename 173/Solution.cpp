0b68bdc0b9d440425af78718d51b203511ea573b
xiaowangfei <56806687+xiaowangfei@users.noreply.github.com>
2020-02-09 17:49:02 -0600

    173. Binary Search Tree Iterator

--- 173/Solution.cpp
+++ 173/Solution.cpp
@@ -10,6 +10,7 @@ BSTIterator(TreeNode* root) {
 /** @return the next smallest number */
 int next() {
     TreeNode* top = S.top();
+    //Pop immediately after visit
     S.pop();
     ptr = top -> right;
     while(ptr){
@@ -18,6 +19,31 @@ int next() {
     }
     return top -> val;
 }
+
+//Use stack of full path. Doesn't pop immediately after visit.
+int next() {
+    TreeNode* top = S.top();
+    int result = top -> val;
+    //Successor case 1: current node has right child
+    if(top -> right){
+        top = top -> right;
+        while(top){
+            S.push(top);
+            top = top -> left;
+        }
+    }
+    //Successor case 2: current node doesn't have right child
+    else{
+        S.pop();
+        while(!S.empty() && top == S.top() -> right){
+            top = S.top();
+            S.pop();
+        }
+    }
+    return result;
+}
+
+
     
 /** @return whether we have a next smallest number */
 bool hasNext() {
